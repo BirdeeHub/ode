b0cdc1e9a9f6e85fc55729a82116a7022db92786
birdee <birdee@birdee.com>
2024-11-22 02:27:23 -0800

    Is there an award for this?
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/main.rs
    	modified:   test.aa

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,118 @@
 use std::fs::File;
 use std::io::{self, Read};
 
+#[derive(Debug, PartialEq)]
+enum Token {
+    If,
+    Else,
+    Identifier(String),
+    Number(i32),
+    Plus,
+    Minus,
+    Equals,
+    LParen,
+    RParen,
+    Eof, // End of file/input
+    Unknown(char),
+}
+
+struct Tokenizer<'a> {
+    input: &'a str,
+    position: usize,
+}
+
+impl<'a> Tokenizer<'a> {
+    fn new(input: &'a str) -> Tokenizer<'a> {
+        Tokenizer {
+            input,
+            position: 0,
+        }
+    }
+
+    fn get_char(&self) -> Option<char> {
+        self.input[self.position..].chars().next()
+    }
+
+    fn advance(&mut self) {
+        self.position += self.get_char().unwrap_or_default().len_utf8();
+    }
+
+    fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+
+        while let Some(c) = self.get_char() {
+            let token = match c {
+                ' ' | '\n' | '\t' => {
+                    self.advance();
+                    continue; // Skip whitespace
+                }
+                '+' => {
+                    self.advance();
+                    Token::Plus
+                }
+                '-' => {
+                    self.advance();
+                    Token::Minus
+                }
+                '=' => {
+                    self.advance();
+                    Token::Equals
+                }
+                '(' => {
+                    self.advance();
+                    Token::LParen
+                }
+                ')' => {
+                    self.advance();
+                    Token::RParen
+                }
+                '0'..='9' => {
+                    let number = self.consume_number();
+                    Token::Number(number)
+                }
+                'a'..='z' | 'A'..='Z' => {
+                    let identifier = self.consume_identifier();
+                    match identifier.as_str() {
+                        "if" => Token::If,
+                        "else" => Token::Else,
+                        _ => Token::Identifier(identifier),
+                    }
+                }
+                _ => {
+                    self.advance();
+                    Token::Unknown(c)
+                }
+            };
+            tokens.push(token);
+        }
+
+        tokens.push(Token::Eof); // End of file
+        tokens
+    }
+
+    fn consume_number(&mut self) -> i32 {
+        let start = self.position;
+        while let Some(c) = self.get_char() {
+            if !c.is_ascii_digit() {
+                break;
+            }
+            self.advance();
+        }
+        self.input[start..self.position].parse().unwrap_or(0)
+    }
+
+    fn consume_identifier(&mut self) -> String {
+        let start = self.position;
+        while let Some(c) = self.get_char() {
+            if !c.is_alphanumeric() {
+                break;
+            }
+            self.advance();
+        }
+        self.input[start..self.position].to_string()
+    }
+}
+
 fn read_file(file_path: &str) -> io::Result<String> {
 
     // Open the file
@@ -15,7 +127,7 @@ fn read_file(file_path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
 
     // Print all arguments
@@ -27,8 +139,14 @@ fn main() {
         Err(io::Error::new(io::ErrorKind::InvalidInput, "No file path provided"))
     };
 
-    match contents {
-        Ok(contents) => println!("{}", contents),
-        Err(err) => println!("Error: {}", err),
+    let contents = contents?;
+
+    let mut tokenizer = Tokenizer::new(&contents);
+    let tokens = tokenizer.tokenize();
+
+    for token in tokens {
+        println!("{:?}", token);
     }
+
+    Ok(())
 }
