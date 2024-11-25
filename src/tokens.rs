651cc85210bbebd6dc6f5fe6e96b1548976ecbd6
birdee <birdee@birdee.com>
2024-11-24 17:43:59 -0800

    eppic fail Robert
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/main.rs
    	modified:   src/tokens.rs

--- src/tokens.rs
+++ src/tokens.rs
@@ -15,7 +15,7 @@ fn is_literal_right_frag(op: &str) -> bool {
     op == "]" || op.starts_with("]") && op.len() > 1 && op[1..].chars().all(|c| c == '=')
 }
 
-pub struct Ops<'a> {
+struct Ops<'a> {
     pub blockcomstart: &'a str,
     pub blockcomend: &'a str,
     pub linecom: &'a str,
@@ -41,7 +41,7 @@ pub struct TokenizerSettings<'a> {
 }
 
 impl<'a> Ops<'a> {
-    pub fn new(options: &'a TokenizerSettings<'a>) -> Ops<'a> {
+    fn new(options: &'a TokenizerSettings<'a>) -> Ops<'a> {
         let mut combined_ops = vec![
             options.blockcomstart,
             options.blockcomend,
@@ -79,10 +79,10 @@ impl<'a> Ops<'a> {
         }
     }
 
-    pub fn is(&self, op: &str) -> bool {
+    fn is(&self, op: &str) -> bool {
         self.ops.contains(&op) || Ops::is_literal_left(op) || Ops::is_literal_right(op)
     }
-    pub fn is_fragment(&self, op: &str) -> bool {
+    fn is_fragment(&self, op: &str) -> bool {
         self.ops
             .iter()
             .any(|&op_def| op_def != op && op_def.starts_with(op))
@@ -90,33 +90,238 @@ impl<'a> Ops<'a> {
             || is_literal_right_frag(op)
     }
 
-    pub fn is_template_op(&self, op: &str) -> bool {
+    fn is_template_op(&self, op: &str) -> bool {
         self.templops.contains(&op)
     }
 
-    pub fn is_other_capturing(&self, op: &str) -> bool {
+    fn is_other_capturing(&self, op: &str) -> bool {
         self.capops.contains(&op) || self.templops.contains(&op)
     }
 
-    pub fn is_left_encloser(&self, op: &str) -> bool {
+    fn is_left_encloser(&self, op: &str) -> bool {
         self.enclosers.iter().any(|(left, _)| *left == op)
     }
 
-    pub fn is_right_encloser(&self, op: &str) -> bool {
+    fn is_right_encloser(&self, op: &str) -> bool {
         self.enclosers.iter().any(|(right, _)| *right == op)
     }
 
-    pub fn is_literal_left(op: &str) -> bool {
+    fn is_literal_left(op: &str) -> bool {
         op.starts_with("[")
             && op.len() > 1
             && (op.ends_with("[") && op[1..op.len() - 1].chars().all(|c| c == '='))
     }
-    pub fn get_literal_left(left_lit_op: &str) -> String {
+    fn get_literal_left(left_lit_op: &str) -> String {
         left_lit_op.replace("[", "]")
     }
-    pub fn is_literal_right(op: &str) -> bool {
+    fn is_literal_right(op: &str) -> bool {
         op.starts_with("]")
             && op.len() > 1
             && (op.ends_with("]") && op[1..op.len() - 1].chars().all(|c| c == '='))
     }
 }
+
+pub struct Tokenizer<'a> {
+    input: &'a str,
+    position: usize,
+    ops_struct: Ops<'a>,
+    in_template: bool,
+    options: &'a TokenizerSettings<'a>,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(input: &'a str, options: &'a TokenizerSettings<'a>, in_template: bool) -> Tokenizer<'a> {
+        Tokenizer {
+            input,
+            position: 0,
+            ops_struct: Ops::new(options),
+            in_template,
+            options,
+        }
+    }
+
+    fn get_char(&self) -> Option<char> {
+        self.input[self.position..].chars().next()
+    }
+
+    fn advance(&mut self) {
+        self.position += self.get_char().unwrap_or_default().len_utf8();
+    }
+
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+        let mut is_templ_literal = self.in_template;
+        let mut level = 0;
+        while let Some(c) = self.get_char() {
+            let token = match c {
+                _ if self.in_template && is_templ_literal => {
+                    is_templ_literal = false;
+                    self.consume_capturing(&mut tokens, self.ops_struct.interstart)
+                },
+                _ if self.ops_struct.is(&c.to_string())
+                    || self.ops_struct.is_fragment(&c.to_string()) =>
+                {
+                    let op = self.consume_op();
+                    match op {
+                        _ if op == self.ops_struct.blockcomstart => {
+                            self.consume_comment(true);
+                            continue;
+                        }
+                        _ if op == self.ops_struct.linecom => {
+                            self.consume_comment(false);
+                            continue;
+                        }
+                        _ if Ops::is_literal_left(&op) => {
+                            tokens.push(Token::Op(op.clone()));
+                            self.consume_literal(&mut tokens, &op)
+                        }
+                        _ if self.ops_struct.is_other_capturing(&op) => {
+                            tokens.push(Token::Op(op.clone()));
+                            self.consume_capturing(&mut tokens, &op)
+                        }
+                        _ if self.in_template && self.ops_struct.is_right_encloser(&op) || self.ops_struct.interend == op => {
+                            if level == 0 {
+                                is_templ_literal = true;
+                            } else {
+                                level -= 1;
+                            }
+                            Token::Op(op)
+                        }
+                        _ if self.in_template && self.ops_struct.is_left_encloser(&op) => {
+                            level += 1;
+                            Token::Op(op)
+                        }
+                        _ if self.ops_struct.is(&op) => {
+                            Token::Op(op)
+                        }
+                        _ => Token::Identifier(op),
+                    }
+                }
+                _ if c.is_whitespace() => {
+                    self.advance();
+                    continue; // Skip whitespace
+                }
+                '0'..='9' => {
+                    let number = self.consume_numeric();
+                    Token::Numeric(number)
+                }
+                _ => Token::Identifier(self.consume_identifier()),
+            };
+            tokens.push(token);
+        }
+        if ! self.in_template {
+            tokens.push(Token::Eof); // End of file
+        }
+        tokens
+    }
+
+    fn consume_comment(&mut self, block: bool) {
+        let endchar = if block { self.ops_struct.blockcomend } else { "\n" };
+        while let Some(_c) = self.get_char() {
+            let remaining = &self.input[self.position..];
+            if remaining.starts_with(endchar) {
+                let mut count = 0;
+                while count < endchar.len() {
+                    self.advance();
+                    count += 1;
+                }
+                break;
+            }
+            self.advance();
+        }
+    }
+    fn consume_literal(&mut self, tokens: &mut Vec<Token>, start_encloser: &str) -> Token {
+        let end_encloser = Ops::get_literal_left(start_encloser);
+        let mut literal = String::new();
+        while let Some(c) = self.get_char() {
+            let remaining = &self.input[self.position..];
+            if remaining.starts_with(&end_encloser) {
+                let mut count = 0;
+                while count < end_encloser.len() {
+                    count += 1;
+                    self.advance();
+                }
+                break;
+            }
+            self.advance();
+            literal.push(c);
+        }
+        tokens.push(Token::Literal(literal));
+        Token::Op(end_encloser)
+    }
+    fn consume_capturing(&mut self, tokens: &mut Vec<Token>, end_encloser: &str) -> Token {
+        let mut literal = String::new();
+        let mut is_escaped = false;
+        while let Some(c) = self.get_char() {
+            let remaining = &self.input[self.position..];
+            if remaining.starts_with(end_encloser) && ! is_escaped {
+                let mut count = 0;
+                while count < end_encloser.len() {
+                    count += 1;
+                    self.advance();
+                }
+                break;
+            }
+            self.advance();
+            is_escaped = c == '\\';
+            if is_escaped && self.input[self.position..].starts_with(end_encloser) {
+            } else {
+                literal.push(c);
+            }
+        }
+        if ! self.in_template || self.get_char().is_some() {
+            if self.ops_struct.is_template_op(end_encloser) {
+                let format_tokens = Tokenizer::new(&literal, self.options, true).tokenize();
+                tokens.push(Token::Format(format_tokens));
+            } else {
+                tokens.push(Token::Literal(literal));
+            }
+            Token::Op(end_encloser.to_string())
+        } else if self.ops_struct.is_template_op(end_encloser) {
+            let format_tokens = Tokenizer::new(&literal, self.options, true).tokenize();
+            Token::Format(format_tokens)
+        } else {
+            Token::Literal(literal)
+        }
+    }
+    fn consume_op(&mut self) -> String {
+        let start = self.position;
+        let mut buffer = String::new();
+        while let Some(c) = self.get_char() {
+            buffer.push(c);
+            if !(self.ops_struct.is(buffer.as_str())
+                || self.ops_struct.is_fragment(buffer.as_str()))
+            {
+                break;
+            }
+            self.advance();
+        }
+        self.input[start..self.position].to_string()
+    }
+    fn consume_numeric(&mut self) -> String {
+        let start = self.position;
+        let mut is_float = false;
+        while let Some(c) = self.get_char() {
+            if !(c.is_ascii_digit() || c == '.') || (c == '.' && is_float) {
+                break;
+            }
+            is_float = c == '.' || is_float;
+            self.advance();
+        }
+        self.input[start..self.position].to_string()
+    }
+    fn consume_identifier(&mut self) -> String {
+        let start = self.position;
+        while let Some(c) = self.get_char() {
+            if self.ops_struct.is(&c.to_string())
+                || self.ops_struct.is_fragment(&c.to_string())
+                || c.is_whitespace()
+            {
+                break;
+            }
+            self.advance();
+        }
+        self.input[start..self.position].to_string()
+    }
+}
+
