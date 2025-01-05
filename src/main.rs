9592e197d21254d1ca5312fc83d7c66758391524
BirdeeHub <birdee@localhost>
2025-01-05 05:03:46 -0800

    I really should've committed this when I finished it...
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,11 @@ mod parser;
 mod runtime;
 use crate::parser::Parser;
 
-struct CharIterator {
-    reader: File,
-}
-
-impl CharIterator {
-    fn new(path: &str) -> io::Result<Self> {
-        let reader = File::open(path)?;
-        Ok(CharIterator { reader })
-    }
-}
-
-//TODO: figure out how to actually make this get chars and not 1 byte
-impl Iterator for CharIterator {
-    type Item = char;
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut buffer = [0; 1];
-        match self.reader.read(&mut buffer) {
-            Ok(0) => None,
-            Ok(_) => {
-                char::from_u32(buffer[0] as u32).map(Some).unwrap_or(None)
-            }
-            Err(_) => None,
-        }
-    }
+fn read_file(file_path: &str) -> io::Result<String> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
 }
 
 fn main() -> io::Result<()> {
@@ -36,8 +17,8 @@ fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     println!("Arguments: {:?}", args);
     let inputvar = args.get(1).expect("No input file provided!");
-    let contents = CharIterator::new(inputvar)?;
-    let mut parser = Parser::new(contents);
+    let contents = read_file(inputvar)?;
+    let mut parser = Parser::new(contents.chars());
     let ast = parser.parse_program().unwrap();
     let rtvals = runtime::evaluate(&ast);
     println!("{:?}", rtvals);
